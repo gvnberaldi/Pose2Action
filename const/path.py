6530c1ff976245c7f52b7e9588d80439c77174f5
gvnberaldi <giovanniberaldi43@gmail.com>
2024-05-29 11:53:43 +0200

    path updated

--- const/path.py
+++ const/path.py
@@ -1,4 +1,4 @@
 import os.path
 
-EXPERIMENTS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..\\experiments')
-ITOP_SIDE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..\\datasets/ITOP-CLEAN-NEW/SIDE')
+EXPERIMENTS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../experiments')
+ITOP_SIDE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../datasets/ITOP-CLEAN-NEW/SIDE')
