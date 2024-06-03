18fabcc1490335ba6b43ff9187d10d15f22650e8
gvnberaldi <giovanniberaldi43@gmail.com>
2024-06-03 20:26:20 +0200

    Dataset path modified

--- const/path.py
+++ const/path.py
@@ -2,4 +2,4 @@ import os.path
 
 EXPERIMENTS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../experiments')
 ITOP_SIDE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../datasets/ITOP-CLEAN-NEW/SIDE')
-BAD_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../datasets/BAD')
+BAD_PATH = '/gpfs/data/fs71186/beraldi/bad_dataset'
